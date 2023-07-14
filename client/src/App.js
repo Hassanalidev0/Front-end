b59a8f8c5ada96973dc49db4fb38537570553e04
Hassan Ali <hassanali.dev0@gmail.com>
2023-07-14 12:59:19 +0500

    First Push

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>First time trying out commitzen plugin github </h1>
+    </div>
+  );
+}
+
+export default App;
